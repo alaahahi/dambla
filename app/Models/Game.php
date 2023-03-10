1e5acee06943d94ce54b5a3039e9fe1e66f2c199
alaahahi <alaa.hahe@gmail.com>
2023-03-10 03:56:47 -0800

    add

---
+++ app/Models/Game.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use TCG\Voyager\Traits\Translatable;
+
+class Game extends Model
+{
+    use HasFactory;
+    use Translatable;
+    protected $table = 'game'; 
+    protected $translatable = ['group_price', 'accumulated'];
+}
